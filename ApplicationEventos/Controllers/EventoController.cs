b1d3ca452ed0b38f7db036288c8446ce86ebb6cb
Mauricio <bolitto@gmail.com>
2023-04-29 12:39:45 -0500

    Borrar y Detalles

--- ApplicationEventos/Controllers/EventoController.cs
+++ ApplicationEventos/Controllers/EventoController.cs
@@ -114,5 +114,83 @@ namespace ApplicationEventos.Controllers
             return View(evento);
         }
 
+        [HttpGet]
+        [Route("Evento/Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Eventos EventosGetAll = new Eventos();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(BaseUrl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage Res = await client.GetAsync($"https://localhost:7242/api/Evento/{id}");
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var _ClientResponse = Res.Content.ReadAsStringAsync().Result;
+                        EventosGetAll = JsonConvert.DeserializeObject<Eventos>(_ClientResponse);
+                        return View(EventosGetAll);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("Evento/Delete/{id}")]
+        public async Task<IActionResult> Delete(int id, [Bind("Id, Lugar, Descripcion, Fecha, Nroentrada, Precio, Estado, EstadoDesc")] Eventos evento)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                //var putTask = client.PutAsync($"api/Evento/{evento.Id}", content);
+                var fechaQuery = evento.Fecha.ToString("dd-MM-yyyy");
+                var putTask = client.DeleteAsync ($"api/Evento/{evento.Id}");
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error, contactar al administrador.");
+            return View(evento);
+        }
+
+        [HttpGet]
+        [Route("Evento/Detalles/{id}")]
+        public async Task<IActionResult> Detalles(int id)
+        {
+            Eventos EventosGetAll = new Eventos();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(BaseUrl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage Res = await client.GetAsync($"https://localhost:7242/api/Evento/{id}");
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var _ClientResponse = Res.Content.ReadAsStringAsync().Result;
+                        EventosGetAll = JsonConvert.DeserializeObject<Eventos>(_ClientResponse);
+                        return View(EventosGetAll);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
